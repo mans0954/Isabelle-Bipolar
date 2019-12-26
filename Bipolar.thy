f733cf110959415f7ca024ac431d1919bf8f5704
Christopher Hoskin <christopher.hoskin@overleaf.com>
2019-12-26 17:47:13 +0000

    Use a character avaliable by default

--- Bipolar.thy
+++ Bipolar.thy
@@ -6,15 +6,15 @@ begin
 
 locale bilinear =
   fixes prod :: "'a::real_vector \<Rightarrow> 'b::real_vector \<Rightarrow> real"
-    (infixl "\<currency>" 70)
+    (infixl "\<star>" 70)
   assumes add_left: "prod (a + a') b = prod a b + prod a' b"
     and add_right: "prod a (b + b') = prod a b + prod a b'"
     and scaleR_left: "prod (scaleR r a) b = scaleR r (prod a b)"
     and scaleR_right: "prod a (scaleR r b) = scaleR r (prod a b)"
 
 locale strict_duality = bilinear +
-  assumes left_strict: "\<lbrakk>x \<currency> y = 0\<rbrakk> \<Longrightarrow> x=0"
-    and right_strict:  "\<lbrakk>x \<currency> y = 0\<rbrakk> \<Longrightarrow> y=0"
+  assumes left_strict: "\<lbrakk>x \<star> y = 0\<rbrakk> \<Longrightarrow> x=0"
+    and right_strict:  "\<lbrakk>x \<star> y = 0\<rbrakk> \<Longrightarrow> y=0"
 
 
 end
